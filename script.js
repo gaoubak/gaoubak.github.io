2cf7b535e7a74873b06e55a829e454c622567804
gaoubak <80465380+gaoubak@users.noreply.github.com>
2021-03-18 12:10:45 +0100

    site presque fini

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector(".btn-menu")
+var btn1 = document.querySelector(".btn-menu1")
+var navbar = document.querySelector("#navbar")
+
+btn.onclick = function () {
+    navbar.classList.toggle("open")
+}
+btn1.onclick = function () {
+    navbar.classList.toggle("open")
+}
